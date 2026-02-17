f7da4cb46876c6a8b9ac3359cd24e38d9ab41426
egyar11 <egyar11@nottingham.ac.uk>
2026-02-17 15:27:21 +0000

    Add files via upload

---
+++ Alexander_Richardson_20734724.m
@@ -0,0 +1,33 @@
+% Alexander Richardson
+% egyar11@nottingham.ac.uk
+
+
+%% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
+
+% Insert answers here
+
+%% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+
+% Insert answers here
+
+%% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
+
+% Insert answers here
+
+
+%% TASK 3 - ALGORITHMS – TEMPERATURE PREDICTION [30 MARKS]
+
+% Insert answers here
+
+
+%% TASK 4 - REFLECTIVE STATEMENT [5 MARKS]
+
+% No need to enter any answers here, please answer on the .docx template.
+
+
+%% TASK 5 - COMMENTING, VERSION CONTROL AND PROFESSIONAL PRACTICE [15 MARKS]
+
+% No need to enter any answers here, but remember to:
+% - Comment the code throughout.
+% - Commit the changes to your git repository as you progress in your programming tasks.
+% - Hand the Arduino project kit back to the lecturer with all parts and in working order.
