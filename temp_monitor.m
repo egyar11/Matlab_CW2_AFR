3afd31e630c50bb60b217445cd1d9e6d49be80cd
egyar11 <egyar11@nottingham.ac.uk>
2026-03-26 22:19:27 +0000

    Task 2 completed! Yippeee! :D

--- temp_monitor.m
+++ temp_monitor.m
@@ -1,13 +1,32 @@
-% define function
 function temp_monitor(a)
+    % TEMP_MONITOR monitors the temperature in real time and indicates if it is
+    % within the acceptable range. 
+    % 
+    % Every second the voltage is measured through a thermistor that is 
+    % connected to an arduino and calculates the thermistor temperature. A 
+    % graph of temperature against time is plotted and updated live. When the 
+    % temperature is within the acceptable range of 18-24 degrees C, a green 
+    % LED is lit, when it is below the range a yellow LED blinks at 0.5s 
+    % intervals, when it is above the range a red LED blinks at 0.25s 
+    % intervals. The arduino object is passed as an argument.
+
+    % create variable to hold time in seconds since the start of the 
+    % function for each reading and set it to 0
+    time = 0;
+    % get first reading for temperature
+    % read voltage from thermistor
+    voltage = readVoltage(a,"A0");
+    % calculate temperature using current voltage of thermistor, 0 
+    % degree voltage ofthermistor, and temperature coefficient of 
+    % thermistor
+    temperature = (voltage - 0.5) / 0.01;
+    % plot graph of temperature against time
+    graph = animatedline(time,temperature);
+    % create axis labels including units
+    xlabel('Time (s)')
+    ylabel('Temperature (°C)')
     % loop indefinitely
     while true
-        % read voltage from thermistor
-        voltage = readVoltage(a,"A0");
-        % calculate temperature using current voltage of thermistor, 0 
-        % degree voltage ofthermistor, and temperature coefficient of
-        % thermistor
-        temperature = (voltage - 0.5) / 0.01;
         % if temperature is below 18 degrees C blink yellow LED at 0.5s
         % intervals for 1 second
         if temperature < 18
@@ -50,5 +69,16 @@ function temp_monitor(a)
             % wait one second
             pause(1)
         end
+        % read voltage from thermistor
+        voltage = readVoltage(a,"A0");
+        % calculate temperature using current voltage of thermistor, 0 
+        % degree voltage ofthermistor, and temperature coefficient of
+        % thermistor
+        temperature = (voltage - 0.5) / 0.01;
+        % increment time
+        time = time + 1;
+        % update graph
+        addpoints(graph,time,temperature)
+        drawnow
     end
 end
